219a5122f0ccd7c2b9aa96ea84272ba1770841c2
Andrei Tuta <tuta.andrei96@gmail.com>
2020-04-10 16:46:40 +0100

    Reflect001: Spring setup
    - Add security util
    - Add password encoding and decoding for signup/login

--- src/main/java/com/andreituta/controller/UserController.java
+++ src/main/java/com/andreituta/controller/UserController.java
@@ -2,11 +2,13 @@ package com.andreituta.controller;
 
 import com.andreituta.model.User;
 import com.andreituta.model.repository.UserRepository;
+import com.andreituta.model.security.SecurityUtil;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +19,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private SecurityUtil securityUtil;
+    // Constants
     private final String INVALID_CRED = "No username/ password passed. Nothing has been executed";
 
     @RequestMapping(value = "/api/user", method = RequestMethod.POST)
@@ -24,8 +29,8 @@ public class UserController {
     public String addUsers(@RequestParam final String username, @RequestParam final String password) {
         if (!StringUtils.isEmpty(username) && !StringUtils.isEmpty(password)) {
             User user = new User();
-            user.setName(username);
-            user.setPassword(password);
+            user.setName(securityUtil.encode(username));
+            user.setPassword(securityUtil.encode(password));
             userRepository.save(user);
             return "Adding username: " + username + " password: " + password;
         }
@@ -33,12 +38,26 @@ public class UserController {
 
     }
 
-    @RequestMapping(value = "/api/user", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/user/{username}/{password}", method = RequestMethod.GET)
     @ResponseBody
-    public String logUsers(@RequestParam final String username, @RequestParam final String password) {
+    public String logUsers(@PathVariable(value = "username") final String username, @PathVariable(value = "password") final String password) {
         if (!StringUtils.isEmpty(username) && !StringUtils.isEmpty(password)) {
             User user = StreamSupport.stream(userRepository.findAll().spliterator(), false)
-                    .filter(u -> u.getName().equals(username) && u.getPassword().equals(password)).findAny().orElse(null);
+                    .filter(u -> securityUtil.decode(username, u.getName()) && securityUtil.decode(password, u.getPassword())).findAny().orElse(null);
+            if (user != null) {
+                return "Retrieved username: " + user.getName() + " password: " + user.getPassword();
+            } else {
+                return "Failed returning a user for provided credentials.";
+            }
+        }
+        return INVALID_CRED;
+    }
+
+    @RequestMapping(value = "/api/user/{id}", method = RequestMethod.PUT)
+    @ResponseBody
+    public String udpateUsers(@PathVariable(value = "id") final String id) {
+        if (!StringUtils.isEmpty(id)) {
+            User user = userRepository.findOne(Integer.valueOf(id));
             if (user != null) {
                 return "Retrieved username: " + user.getName() + " password: " + user.getPassword();
             } else {
