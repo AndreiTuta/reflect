729b32e010e3725c1fa4c62383fb548b60c75fe4
andreituta <tuta.andrei96@gmail.com>
2020-05-14 19:41:30 +0100

    reflect-5: Security
    	- Add JWT tokens and implementation

---
+++ src/main/java/com/at/reflect/common/utils/Util.java
@@ -0,0 +1,5 @@
+package com.at.reflect.common.utils;
+
+public interface Util {
+
+}
---
+++ src/main/java/com/at/reflect/controller/service/UserService.java
@@ -0,0 +1,12 @@
+package com.at.reflect.controller.service;
+
+import com.at.reflect.model.entity.User;
+
+public class UserService {
+
+	public User loadUserByUsername(String usernameFromToken) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
