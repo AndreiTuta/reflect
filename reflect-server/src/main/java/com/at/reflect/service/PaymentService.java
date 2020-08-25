08125032857abb4607a84ca6ddd497ff2b2984c6
andreituta <tuta.andrei96@gmail.com>
2020-08-25 19:53:26 +0100

    Create actual fetch and update methods for payment

--- reflect-server/src/main/java/com/at/reflect/service/PaymentService.java
+++ reflect-server/src/main/java/com/at/reflect/service/PaymentService.java
@@ -1,13 +1,18 @@
 package com.at.reflect.service;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import com.at.reflect.dao.PaymentDao;
+import com.at.reflect.error.exception.NotFoundException;
+import com.at.reflect.error.exception.PathException;
 import com.at.reflect.model.request.PaymentRequest;
 import com.at.reflect.model.response.PaymentResponse;
+import com.reflect.generated.tables.pojos.Meditation;
 import com.reflect.generated.tables.pojos.Payment;
 
 import lombok.RequiredArgsConstructor;
@@ -20,17 +25,32 @@ public class PaymentService implements Service {
     private final ModelMapper modelMapper;
 
     private PaymentResponse.PaymentResponseBuilder buildPaymentResponse(final Payment payment) {
-        return PaymentResponse.builder();
+        return PaymentResponse.builder()
+                              .id(payment.getId())
+                              .amount(payment.getAmount())
+                              .transactionId(payment.getTransactionId())
+                              .created(payment.getCreated())
+                              .amount(payment.getAmount());
     }
 
     @Override
     public ServiceType getType() {
-        return ServiceType.USER;
+        return ServiceType.PAYMENT;
     }
 
-    public void updatePayment(String paymentId, @Valid PaymentRequest paymentRequest) {
-        // TODO Auto-generated method stub
+    public PaymentResponse updatePayment(String paymentId, @Valid PaymentRequest paymentRequest) throws NotFoundException {
+        try {
+            int id = Integer.parseInt(paymentId);
+            fetchOptionalPaymentById(id)
+                                   .orElseThrow(() -> new NotFoundException("Payment with id: " + paymentId + " not found"));
 
+            final Payment payment = modelMapper.map(paymentRequest, Payment.class);
+            payment.setId(id);
+            paymentDao.update(payment);
+            return buildPaymentResponse(payment).build();
+        } catch (NumberFormatException e) {
+            throw new PathException("meditationId on path must be an integer");
+        }
     }
 
     public PaymentResponse delete(String paymentId) {
@@ -38,9 +58,20 @@ public class PaymentService implements Service {
         return null;
     }
 
-    public PaymentResponse fetchPaymentById(String paymentId) {
-        // TODO Auto-generated method stub
-        return null;
+    public PaymentResponse fetchPaymentById(String paymentId) throws NotFoundException {
+        try {
+            int id = Integer.parseInt(paymentId);
+            final Payment payment = fetchOptionalPaymentById(id)
+                                                                .orElseThrow(() -> new NotFoundException("Payment with id: "
+                                                                    + paymentId + " not found"));
+            return buildPaymentResponse(payment).build();
+        } catch (NumberFormatException e) {
+            throw new PathException("paymentId on path must be an integer");
+        }
+    }
+
+    public Optional<Payment> fetchOptionalPaymentById(final Integer paymentId) {
+        return Optional.ofNullable(paymentDao.findById(paymentId));
     }
 
     public PaymentResponse createPayment(@Valid PaymentRequest paymentRequest) {
